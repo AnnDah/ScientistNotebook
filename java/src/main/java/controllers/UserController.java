efc4439eb2de05722b3903ad844de436c3288009
Nirfelt <sebastian.nirfelt@gmail.com>
2015-04-20 18:06:28 +0200

    Fixed error in UserController

--- java/src/main/java/controllers/UserController.java
+++ java/src/main/java/controllers/UserController.java
@@ -6,9 +6,9 @@ import models.User;
  */
 public class UserController {
 
-    public void createUser(String firstName, String lastName){
+    public void createUser(String firstName, String lastName, String id, String email, String password){
         User user;
-        user = new User(firstName, lastName);
+        user = new User(firstName, lastName, id, email, password);
         System.out.printf("First Name: %s\nLast Name: %s",user.getFirstName(), user.getLastName());
     }
 }

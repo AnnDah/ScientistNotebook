84632341f4a1c61c6d3509811a61ad924e03d429
Annika Magnusson <annika.h.magnusson@gmail.com>
2015-04-20 12:24:15 +0200

    Added variables for user

--- java/src/main/java/models/User.java
+++ java/src/main/java/models/User.java
@@ -6,17 +6,36 @@ package models;
 public class User {
     private String firstName;
     private String lastName;
+    private String id;
+    private String email;
+    private String password;
 
-    public User(String firstName, String lastName){
+    public User(String firstName, String lastName, String id, String email, String password){
         this.firstName = firstName;
         this.lastName = lastName;
+        this.id = id;
+        this.email = email;
+        this.password = password;
     }
 
+    // Getters for class variables
     public String getFirstName(){
         return this.firstName;
     }
 
     public String getLastName(){
         return this.lastName;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getPassword(){
+        return this.password;
     }
 }

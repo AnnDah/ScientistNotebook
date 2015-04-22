e5d01c269b737fc1cd0efc9d246f0a72511c2c2e
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2015-04-22 11:55:33 +0200

    Added functionality to Data.java

--- java/src/main/java/models/Data.java
+++ java/src/main/java/models/Data.java
@@ -4,4 +4,23 @@ package models;
  * Created by annikamagnusson on 20/04/15.
  */
 public class Data {
+    private String id;
+    private String content;
+    private String author;
+    private String visibility;
+
+    public Data(String id, String content, String author, String visibility){
+        this.id = id;
+        this.content = content;
+        this.author = author;
+        this.visibility = visibility;
+    }
+
+    public String GetId(){ return this.id; }
+
+    public String GetContent() { return this.content; }
+
+    public String GetAuthor() { return this.author; }
+
+    public String GetVisibility { return this.visibility; }
 }

0e183f6a4c8a3aba463ddd7b2c25c5e7b3febe12
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2015-04-22 10:40:26 +0200

    Added base for db connection

--- java/src/main/java/models/DatabaseConnector.java
+++ java/src/main/java/models/DatabaseConnector.java
@@ -4,4 +4,21 @@ package models;
  * Created by annikamagnusson on 20/04/15.
  */
 public class DatabaseConnector {
+    //Cluster cluster;
+    //Session session;
+
+    private String dbaddress = "";
+    private String keyspace = "";
+
+    // Connect to the cluster and keyspace "demo"
+    //cluster = Cluster.builder().addContactPoint(dbaddress).build();
+    //session = cluster.connect(keyspace);
+
+    public User GetUser(){
+        return null;
+    }
+
+    public Data GetData(){
+        return null;
+    }
 }

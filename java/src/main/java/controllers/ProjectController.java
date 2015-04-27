4489fe60de3fd736869dbf5766cbb177d0d3861c
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2015-04-27 12:37:31 +0200

    Added organizationController

--- java/src/main/java/controllers/ProjectController.java
+++ java/src/main/java/controllers/ProjectController.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import org.json.simple.JSONObject;
+
 /**
  * Created by annikamagnusson on 20/04/15.
  */
 public class ProjectController {
 
-
+    public JSONObject GetProject(){
+        return null;
+    }
 }

b6d7781fd5625b446c89ff81567b8e428f83bee9
Annika Magnusson <annika.h.magnusson@gmail.com>
2015-05-08 11:20:14 +0200

    Added function to get and delete project

--- java/src/main/java/controllers/ProjectController.java
+++ java/src/main/java/controllers/ProjectController.java
@@ -73,13 +73,82 @@ public class ProjectController {
         db.close();
     }
 
-    public JSONObject getProject(String id){
-        JSONObject j = new JSONObject();
-        return j;
+    public JSONObject getProject(String projectId){
+        if (projectId == null){
+            System.out.println("No request parameter was provided");
+            return null;
+        }
+        DatabaseConnector db = new DatabaseConnector();
+        db.connectDefault();
+        Project project;
+
+        Mapper<Project> mapper = new MappingManager(db.getSession()).mapper(Project.class);
+        project = mapper.get(projectId);
+        if (project == null) {
+            System.out.println("Project wasn't found in database");
+            db.close();
+            return null;
+        }
+
+        String id = project.getId();
+        String field = project.getField();
+        List<String> tags = project.getTags();
+        String sharedLevel = project.getSharedLevel();
+        String projectAbstract = project.getProjectAbstract();
+        List<String> participants = project.getParticipants();
+        List<String> projectRoles = project.getProjectRoles();
+        String author = project.getAuthor();
+        String name = project.getName();
+        List<String> followers = project.getFollowers();
+        String status = project.getStatus();
+
+        JSONObject projectJson = createProjectJson(id, field, tags, sharedLevel, projectAbstract, participants,
+                projectRoles, author, name, followers, status);
+        db.close();
+        return projectJson;
     }
 
-    public int deleteProject(String id){
+    public int deleteProject(String projectId){
+        if (projectId == null){
+            System.out.println("No request parameter was provided");
+            return 400;
+        }
+        DatabaseConnector db = new DatabaseConnector();
+        db.connectDefault();
+        try {
+            Mapper<Project> mapper = new MappingManager(db.getSession()).mapper(Project.class);
+            Project toDelete = mapper.get(projectId);
+            if (toDelete == null){
+                System.out.println("Project wasn't found in database");
+                db.close();
+                return 404;
+            }
+            mapper.delete(toDelete);
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        db.close();
         return 200;
 
+    }
+
+    public JSONObject createProjectJson(String id, String field, List<String> tags, String sharedLevel, String projectAbstract,
+                                        List<String> participants, List<String> projectRoles, String author, String name,
+                                        List<String> followers, String status){
+        JSONObject projectJson = new JSONObject();
+        projectJson.put("id", id);
+        projectJson.put("field", field);
+        projectJson.put("sharedLevel", sharedLevel);
+        projectJson.put("projectAbstract", projectAbstract);
+        projectJson.put("author", author);
+        projectJson.put("name", name);
+        projectJson.put("status", status);
+        projectJson.put("tags", tags);
+        projectJson.put("participants", participants);
+        projectJson.put("projectRoles", projectRoles);
+        projectJson.put("followers", followers);
+
+        return projectJson;
     }
 }
--- java/src/main/java/controllers/UserController.java
+++ java/src/main/java/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-
+import java.util.Map;
 
 
 /**
